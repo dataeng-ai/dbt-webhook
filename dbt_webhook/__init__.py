deb5c1485a7912fb2789dbaef3e026ff0332bec9
DataEng.ai <dataeng.ai@google.com>
2025-02-28 21:29:50 -0800

    expose data models from package

--- dbt_webhook/__init__.py
+++ dbt_webhook/__init__.py
@@ -1,3 +1,6 @@
-from dbt_webhook.plugin import dbtWebhook
+from .plugin import dbtWebhook
+from .webhook_model import Node, CommandBase, WebhookNode, WebhookCommand
+
+__all__ = ["Node", "CommandBase", "WebhookNode", "WebhookCommand", "dbtWebhook"]
 
 plugins = [dbtWebhook]
