751bc7803237f17a4e425ed9f4b2dd7f40962f1c
DataEng.ai <dataeng.ai@google.com>
2025-05-02 10:54:38 -0700

    auth fix

--- dbt_webhook/dynamic_vars/gcp_id_token.py
+++ dbt_webhook/dynamic_vars/gcp_id_token.py
@@ -4,6 +4,7 @@ import google.auth.transport.requests
 from dbt_webhook import events
 from dbt_webhook.config import baseHookConfig
 from dbt_webhook.dynamic_vars.base import DynamicValueBase
+from google.oauth2 import id_token
 
 class GcpIdentityToken(DynamicValueBase):
     def get_value(self, node_config: baseHookConfig) -> str:
@@ -14,9 +15,9 @@ class GcpIdentityToken(DynamicValueBase):
         request = google.auth.transport.requests.Request()
         if isinstance(credentials, google.oauth2.service_account.Credentials):
             events.debug(events.ServiceAccountIdentityToken(credentials.service_account_email))
-            id_token = google.oauth2.id_token.fetch_id_token(request, audience)
+            token = id_token.fetch_id_token(request, audience)
         else:
             credentials.refresh(request)
-            id_token = credentials.id_token
+            token = credentials.id_token
 
-        return id_token
+        return token
